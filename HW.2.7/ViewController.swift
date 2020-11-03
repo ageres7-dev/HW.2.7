4d56a4b1f12a6f71f899e87bd7ba3443c0654750
Sergey Dolgikh <ageres7@gmail.com>
2020-11-03 14:50:01 +0500

    Initial Commit

---
+++ HW.2.7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW.2.7
+//
+//  Created by Сергей Долгих on 03.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
